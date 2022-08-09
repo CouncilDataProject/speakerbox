01c0198524765880674156418ec178055adbb693
Eva Maxfield Brown <evamaxfieldbrown@gmail.com>
2022-08-09 13:48:37 -0700

    Bump version: 0.0.2 → 0.1.0

--- setup.py
+++ setup.py
@@ -103,6 +103,6 @@ setup(
     url="https://github.com/CouncilDataProject/speakerbox",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="0.0.2",
+    version="0.1.0",
     zip_safe=False,
 )
--- speakerbox/__init__.py
+++ speakerbox/__init__.py
@@ -6,7 +6,7 @@ __author__ = "Eva Maxfield Brown"
 __email__ = "evamaxfieldbrown@gmail.com"
 # Do not edit this string manually, always use bumpversion
 # Details in CONTRIBUTING.md
-__version__ = "0.0.2"
+__version__ = "0.1.0"
 
 
 def get_module_version() -> str:
