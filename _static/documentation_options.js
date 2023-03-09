088323223278b2fdc68167010a59da3dc3a0cb30
evamaxfield <evamaxfieldbrown@gmail.com>
2023-03-09 00:21:30 +0000

    Deploying to gh-pages from @ CouncilDataProject/speakerbox@47c13c70a209cdb081ff85f7bb4ad51f7c1637fb 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+g00ab10c',
+    VERSION: '0.1.dev1+g47c13c7',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
